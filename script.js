42942ec34d646bf8f6f1a168f37e8c4756f0cfb1
unknown <paweladamian@gmail.com>
2021-11-13 13:40:56 +0100

    Change configuration inputs padding

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const InputName = document.querySelector("#JS-inp-name");
-const InputPassword = document.querySelector("#JS-inp-password");
-const InputSubmit = document.querySelector("#JS-btn-confirm");
-
 const burgerBtn = document.querySelector('.burger');
 const mobileNav = document.querySelector('.nav-mobile-list');
 
